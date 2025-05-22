eda8b28dc67ac4ee6ae4299aeac31d3d7d3bacd1
morgarin <37238846+morgarin@users.noreply.github.com>
2025-05-22 23:59:54 +0300

    Add files via upload

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,27 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
 	// TODO: добавить методы
+	Parse(string) error
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
 	// TODO: реализовать функцию
+	for _, data := range dataset {
+		err := dp.Parse(data)
+		if err != nil {
+			log.Println(err)
+		}
+		s, err := dp.ActionInfo()
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println(s)
+	}
 }
--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,51 @@
 package daysteps
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
 	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
 	// TODO: реализовать функцию
+	strings := strings.Split(datastring, ",")
+	if len(strings) != 2 {
+		return errors.New("dsParse: len(strings) != 2")
+	}
+	ds.Steps, err = strconv.Atoi(strings[0])
+	if err != nil {
+		return err
+	}
+	if ds.Steps < 1 {
+		return errors.New("dsParse: steps <= 0")
+	}
+	ds.Duration, err = time.ParseDuration(strings[1])
+	if err != nil {
+		return err
+	}
+	if ds.Duration <= 0 {
+		return errors.New("dsParse: steps <= 0")
+	}
+	return err
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
 	// TODO: реализовать функцию
+	calories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Personal.Weight, ds.Personal.Height, ds.Duration)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", ds.Steps, spentenergy.Distance(ds.Steps, ds.Personal.Height), calories), err
 }
